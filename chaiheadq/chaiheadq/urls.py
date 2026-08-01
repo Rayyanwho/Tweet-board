6d0ea9601075a8f0b403e11567e4d6a4fe381c9f
rayyan <rayyanislive8@gmail.com>
2026-08-01 23:58:01 +0500

    Redirect root URL to tweet page

--- chaiheadq/chaiheadq/urls.py
+++ chaiheadq/chaiheadq/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth import views as auth_views
+from django.views.generic import RedirectView
+
 urlpatterns = [
+    path('', RedirectView.as_view(url='/tweet/', permanent=False)),
     path('admin/', admin.site.urls),
     path('tweet/', include('tweet.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
